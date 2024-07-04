dee3e9cd4bd04d88ba29551847d6b35a0eb8f520
Fábio Barrufi Pagano <61845465+fabio-barrufi@users.noreply.github.com>
2024-07-04 17:39:39 -0300

    Corrigindo classe MongoDB

---
+++ src/main/java/com/payment/challange/infrastructure/MongoDBConfiguration.java
@@ -0,0 +1,25 @@
+package com.payment.challange.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoDBConfiguration {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDbFactory,
+                                                       MongoMappingContext mongoMappingContext) {
+
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
+    }
+}
